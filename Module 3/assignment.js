2467dbb3c07cbfeb5821b2433a5e5269598d5fc4
Molly Staples <mollystaples3@gmail.com>
2023-09-27 11:14:30 -0500

    module 3

--- Module 3/assignment.js
+++ Module 3/assignment.js
@@ -16,50 +16,25 @@ function validateANDadd() {
     else {
         
         //* This is where we add a row and the new word to either list 1 or 2
-        if (theNewNumber==1){
-            let lowercaseWord = theNewWord.toLowerCase()
-            var splitString = lowercaseWord.split("");
-            var reverseArray = splitString.reverse();
-            var joinArray = reverseArray.join("");
-            
-            for (let i = 0, len = lowercaseWord.length, text = ""; i < len; i++){
-                if (lowercaseWord != joinArray){ 
+        if (theNewNumber==1){     
                     var tableRef = document.getElementById("mylist1");
-                    (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + "false"
+                    (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + " " + Palindrome1(theNewWord);
                 }
-                else{
-                    var tableRef = document.getElementById("mylist1");
-                    (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + "true"
-                }
-            }
-
-        }
+                
         else{
-           
-            function isPalindrome() {
-                let lowercaseWord2 = theNewWord.toLowerCase()
-                let splitString = lowercaseWord2.split("");
-                let reverseArray = splitString.reverse();
-                let joinArray = reverseArray.join("");
-                if (theNewNumber == 2){
-                    var tableRef = document.getElementById("mylist2");
-                    (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + "true";
-                }
-                //*else {
-                    //*var tableRef = document.getElementById("mylist1");
-                    //*(tableRef.insertRow(tableRef.rows.length)).innerHTML = lowercaseWord2 + "false";
-                //*}
+                var tableRef = document.getElementById("mylist2");
+                (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + " " + Palindrome2(theNewWord);
             }
             
-        }
+        
         
         document.forms["myform"]["newWord"].value = "";
         document.forms["myform"]["newNumber"].value = "";
         return true;
         
     }
-}
 
+}
 function clearList1() {
     //* This clears list 1 when the user clicks the button
     var tableRef = document.getElementById("mylist1");
@@ -70,4 +45,29 @@ function clearList2() {
     //* This clears list 2 when the user clicks the button
     var tableRef = document.getElementById("mylist2");
     tableRef.innerHTML = " ";
+}
+
+function Palindrome1(newWord) {
+    let lowercaseWord = newWord.toLowerCase()
+    let splitString = lowercaseWord.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    
+    return (lowercaseWord == joinArray)
+}
+
+function Palindrome2(newWord) {
+    newWord = newWord.toLowerCase();
+    let splitString = newWord.split("");
+    splitString.reverse();
+
+
+
+    for(let i = 0; i < newWord.length; i++) {
+        if (splitString [i] != newWord [i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
