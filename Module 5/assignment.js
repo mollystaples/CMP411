bcff353fc4f919e25644751f50499acb6b1f8c19
Molly Staples <mollystaples3@gmail.com>
2023-10-31 19:14:40 -0500

    mod 5

---
+++ Module 5/assignment.js
@@ -0,0 +1,50 @@
+async function getStrings() {
+    //First build the API call string by starting with the URL
+    var apiString= "https://ciprand.p3p.repl.co/api?";
+    //next add the parameters to the string using the drop down lists
+    var theNewLength = document.getElementById("length").value;
+    var theNewNumber = document.getElementById("number").value;
+    
+
+    if (theNewLength==1){
+        var theNewLength = "len=20"
+    }
+    else if (theNewLength==2){
+        var theNewLength = "len=50"
+    } 
+    else {
+        var theNewLength = "len=100"
+    }
+
+    if (theNewNumber==1){
+        var theNewNumber = "&count=5"
+    }
+    else if (theNewNumber==2){
+        var theNewNumber = "&count=10"
+    }
+    else {
+        var theNewNumber = "&count=15"
+    }
+    
+
+    apiString = apiString + theNewLength + theNewNumber;
+    alert(apiString);
+
+    //now make the API call to the web service using the string and store what is returned in response
+    var response = await fetch(apiString);
+
+    
+
+    //finally, print the repsonse in the various formats
+    document.getElementById("strings").innerHTML= "";
+
+    var jsonData = await response.json();
+
+    //stringify and print out the JSON object in the RawData section 
+    document.getElementById("strings").innerHTML = JSON.stringify(jsonData.Strings);
+    
+
+    
+
+    return true;
+}
